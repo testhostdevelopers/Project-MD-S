5c3c2aac0744ac5de24e6301b5d45ad42e1114fe
muhsin.deniz <mnknsro413@gmail.com>
2022-01-02 12:24:05 +0300

    popup and layout edited

--- src/pages/coin.js
+++ src/pages/coin.js
@@ -20,6 +20,9 @@ import config from "../config.json";
 import AccountBalanceMigrate from '../components/AccountBalanceMigrate';
 import TotalMarketInfoStake from '../components/TotalMarketInfo/TotalMarketInfoStake';
 import TotalMarketMigrate from '../components/TotalMarketInfo/TotalMarketMigrate';
+import edenSwap from '../assets/images/coin/edenSwap.png'
+import DOGE from '../assets/images/coin/dogecoin.png'
+import cryptoLogo from '../assets/images/cryptoLogo.png'
 
 const { StakingAddress, DogeAddress, LoriaAddress, DogeRewardAddress, LoriaRewardAddress } = config;
 
@@ -116,15 +119,7 @@ function Coin(props) {
                                         stake={_Stake}
                                     />
                                     :
-                                    stakeBalance === "bonding"
-
-                                        ?
-
-                                        <AcountBalanceBonds />
-
-                                        :
-
-                                        <AccountBalanceMigrate />
+                                    <AcountBalanceBonds />
                             }
 
                         </div>
@@ -136,11 +131,7 @@ function Coin(props) {
                                             stakeBalance === "stake" ?
                                                 "MsDoge Staking"
                                                 :
-                                                stakeBalance === "bonding"
-                                                ?
                                                 "MsDoge Bonding"
-                                                :
-                                                "MsDoge Migrate"
                                         }
                                     </h2>
                                     <p>
@@ -148,11 +139,7 @@ function Coin(props) {
                                             stakeBalance === "stake" ?
                                                 "List of stakes below"
                                                 :
-                                                stakeBalance === "bonding"
-                                                ?
                                                 "List of bonds below"
-                                                :
-                                                "List of migrate below"
                                         }
 
                                     </p>
@@ -169,6 +156,10 @@ function Coin(props) {
                                     <div data-bs-toggle="modal" data-bs-target="#migrateTransactionPopup" className={stakeBalance === "migrate" ? "stake-btn" : stakeBalance === "bonding" ? "stake-btn-outline" : "stake-btn-outline"}>
                                         <a className="btn" onClick={() => setStakeBalance("migrate")} >Migrate</a>
                                     </div>
+
+                                    {/* <div data-bs-toggle="modal" data-bs-target="#migrateTransactionPopup" className={stakeBalance === "migrate" ? "stake-btn" : stakeBalance === "bonding" ? "stake-btn-outline" : "stake-btn-outline"}>
+                                        <a className="btn" onClick={() => setStakeBalance("migrate")} >Migrate</a>
+                                    </div> */}
                                 </div>
                             </div>
 
@@ -187,31 +178,16 @@ function Coin(props) {
                                         stake={_Stake}
                                     />
                                     :
-                                    stakeBalance === "bonding"
+                                    <ListOfBounds
+                                    web3={_web3}
+                                    stake={_Stake}
+                                    dogeB={_dogeBalance}
+                                    loriaB={_loriaBalance}
+                                    loriaCoin={_LoriaCoin}
+                                    coin={_DogeCoin}
+                                    reward={_DogeReward} 
+                                />
 
-                                        ?
-
-                                        <ListOfBounds
-                                            web3={_web3}
-                                            stake={_Stake}
-                                            dogeB={_dogeBalance}
-                                            loriaB={_loriaBalance}
-                                            loriaCoin={_LoriaCoin}
-                                            coin={_DogeCoin}
-                                            reward={_DogeReward} 
-                                        />
-
-                                        :
-
-                                        <ListOfMigrate
-                                        web3={_web3}
-                                        stake={_Stake}
-                                        dogeB={_dogeBalance}
-                                        loriaB={_loriaBalance}
-                                        loriaCoin={_LoriaCoin}
-                                        coin={_DogeCoin}
-                                        reward={_DogeReward} 
-                                        />
                             }
 
                         </div>
@@ -219,6 +195,79 @@ function Coin(props) {
                 </div>
             </div>
             <Footer />
+            { /* MODAL */}
+         <div className="modal fade" id="migrateTransactionPopup" tabIndex="-1" aria-labelledby="msDogeApprove" aria-hidden="true">
+            <div className="modal-dialog">
+               <div className="modal-content migrateTransactionPopupContent">
+                  <div className="modal-body popup-card-container rel">
+                     <header className="d-flex justify-content-between pt-0 pb-0 align-items-center" style={{ background: "none" }}>
+                        <div type="button" className="closebtn">
+                           <img src={edenSwap} width="30" />
+                        </div>
+
+                        <div data-bs-toggle="modal" data-bs-target="#migrateTransactionPopup" className="cursor-pointer position-absolute migrateTransactionClose" style={{ right: "15px", top: "15px" }}>
+                           <i class="fas fa-times-circle"></i>
+                        </div>
+                     </header>
+
+                     <div className="heading-text-popupm">
+                        <div className="input-bal">
+                           <div className="row pt-5">
+
+                              <div className="col-12">
+                                 <div className="mt-3 mb-3">
+                                    <p className="w-100 text-center">
+                                       <div className="w-100 mb-2">
+                                          <span><img src={DOGE} width="48px" /></span>
+                                          <span><img src={cryptoLogo} width="48px" /></span>
+                                       </div>
+                                       <h3 className="mb-2"><b>72.4982</b></h3>
+                                       <h6 className="mb-2"><b>USDC/UNI Pool Tokens</b></h6>
+                                       <small>Learn more about Uniswap, chat with the team, <br />others in the community, Demo text. </small>
+                                    </p>
+                                 </div>
+                              </div>
+
+                              <div className="mb-3 col-sm-12 d-flex justify-content-between">
+                                 <b><small>MSDOGE Deposited:</small></b>
+                                 <b><small>0.00007645</small></b>
+                              </div>
+
+                              <div className="mb-3 col-sm-12 d-flex justify-content-between">
+                                 <b><small>CRYPTO Deposited:</small></b>
+                                 <b><small>1252526</small></b>
+                              </div>
+
+                              <div className="mb-3 col-sm-12 d-flex justify-content-between">
+                                 <b><small>Rates:</small></b>
+                                 <b><small>1 MSDOGE = 0.000088374</small></b>
+                              </div>
+
+                              <div className="mb-3 col-sm-12 d-flex justify-content-between">
+                                 <b><small></small></b>
+                                 <b><small>1 CRYPTO = 162738</small></b>
+                              </div>
+
+                              <div className="mb-3 col-sm-12 d-flex justify-content-between">
+                                 <b><small>Share of pool:</small></b>
+                                 <b><small>0.00007645%</small></b>
+                              </div>
+
+
+                              <div className="col-sm-12">
+                                 <div className="p-2 stake-btn">
+                                    <a type="button" className="table-btn btn w-100">Confirm Transaction</a>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+
+
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
         </>
     )
 }
